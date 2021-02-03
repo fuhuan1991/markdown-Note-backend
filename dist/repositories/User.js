619366490c9d55e665ac4be68da79e0d743f1a11
fuhuan1991 <huanfu2019@gmail.com>
2021-02-02 22:42:18 -0500

    rm

--- dist/repositories/User.js
+++
@@ -1,9 +0,0 @@
-class User {
-    constructor(id, nickname, email) {
-        this.id = id;
-        this.nickname = nickname;
-        this.email = email;
-    }
-}
-module.exports = User;
-//# sourceMappingURL=User.js.map
