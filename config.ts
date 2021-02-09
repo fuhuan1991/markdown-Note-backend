5cd06f648daf72cd4f504cb778b0f1eea9cb1840
fuhuan1991 <huanfu2019@gmail.com>
2021-02-09 13:44:00 -0500

    solve sucrity issue

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ const PORT = 3000;
 
 const AWSConfig = {
   region: "us-east-1",
-  endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+  endpoint: "https://dynamodb.us-east-1.amazonaws.com",
 };
 
 export default {
